7b5f305df3b92e85e8496501af36066490733240
Ian Macalinao <me@ian.pw>
2015-01-05 00:56:24 -0600

    Add error handling

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ var express = require('express');
 var app = express();
 
 app.get('*', function(req, res) {
-  res.type('png');
-  var eq = decodeURIComponent(req.originalUrl.substring(1));
-  mathmode(eq).pipe(res);
+  try {
+    var eq = decodeURIComponent(req.originalUrl.substring(1));
+    var math = mathmode(eq);
+    res.type('png');
+    math.pipe(res);
+  } catch (e) {
+    res.send('Error piping');
+  }
 });
 
 var port = process.env.PORT || 3000;
