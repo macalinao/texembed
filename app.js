918763545729a8cd084b8c4c9bf79fdb6579698d
Ian Macalinao <me@ian.pw>
2015-01-05 00:03:22 -0600

    Make things work

---
+++ app.js
@@ -0,0 +1,16 @@
+var mathmode = require('mathmode');
+var gm = require('gm');
+var express = require('express');
+
+var app = express();
+
+app.get('*', function(req, res) {
+  res.type('png');
+  var eq = decodeURIComponent(req.originalUrl.substring(1));
+  mathmode(eq).pipe(res);
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('Listening on port ' + port);
+});
